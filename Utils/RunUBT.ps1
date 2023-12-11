8d681255c8b7dc727f5be189b3d9cacf405275f3
Michael Delva <michael@emidee.net>
2023-12-11 09:01:52 +0100

    Typo

--- Utils/RunUBT.ps1
+++ Utils/RunUBT.ps1
@@ -1,6 +1,6 @@
 function RunUBT( [string[]] $arguments )
 {
-    $all_arguments = @( $global:context.ProjectInfos.UProjectPath ) + $Arguments
+    $all_arguments = @( $global:context.ProjectInfos.UProjectPath ) + $arguments
     $Process = Start-Process -FilePath $global:context.UBTPath -ArgumentList $all_arguments -NoNewWindow -Wait
     return $Process.ExitCode
 }
