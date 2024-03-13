678b724c8be2eec03993d4765335385ad85a787e
Michael Delva <michael@emidee.net>
2024-03-13 10:57:02 +0100

    Removed unused code

--- UEPoshScripts.psm1
+++ UEPoshScripts.psm1
@@ -1,22 +1,3 @@
-class EngineDefinition
-{
-    [String] $EngineAssociation
-    [Version] $Version
-}
-
-class InstalledEngineDefinition
-{
-    [EngineDefinition] $EngineDefinition
-    [String] $Path
-}
-
-class ProjectInfos
-{
-    [String] $Folder;
-    [String] $Name;
-    [String] $FullPath;
-}
-
 $ScriptFiles = Get-ChildItem -Recurse "$PSScriptRoot\Utils" -Include *.ps1 
  
 foreach ( $ScriptFile in $ScriptFiles ) { 
