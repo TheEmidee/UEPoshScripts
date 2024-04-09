aa95d6540a85bb926dadfa6d0c8aae8c6fe4542c
Michael Delva <michael@emidee.net>
2024-04-09 20:26:52 +0200

    Ask confirmation before closing a running editor

--- Tools/CloseEditor.ps1
+++ Tools/CloseEditor.ps1
@@ -1,3 +1,14 @@
 Import-Module -Name ( Resolve-Path( Join-Path -Path ( $PSScriptRoot ) -ChildPath "..\UEPoshScripts.psm1" ) ) -ErrorAction Stop -Force
 
-Stop-Processes -processName "UnrealEditor*"
+$processName = "UnrealEditor*"
+
+$process = Get-Process -Name $processName -ErrorAction SilentlyContinue
+
+if ( $process ) {
+    $choice = Read-Host "The editor is already running. Do you want to stop it? (Y/N)"
+    
+    # Check the user's response
+    if ( $choice -eq "Y" -or $choice -eq "y" ) {
+        Stop-Processes -processName $processName
+    }
+}
